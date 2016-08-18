70c27c24439f9248942b2e2f7048cbccdc8a7e05
Chris Samuel <ksamuel.chris@icloud.com>
2016-08-18 00:00:23 -0400

    load json data  setup into our app

--- public/javascripts/quiz.js
+++ public/javascripts/quiz.js
@@ -4,6 +4,19 @@
 
 	app.controller('MortyController', ['$scope', '$http','$sce', function($scope,$http,$sce){
 		// the entire application will be controlled within this controller
+		
+		$scope.score = 0;
+		$scope.activeQuestion = -1;
+		$scope.activeQUestionAnswered = 0;
+		$scope.percentage = 0;
+
+		$http.get('quiz_data.json').then(function(quizData){
+			$scope.myQuestions = quizData.data;
+			$scope.totalQuestions =  $scope.myQuestions.length;
+	});
+
+
+
 	}]);
 
 
